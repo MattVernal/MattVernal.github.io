ff3bc3a15a393fc02afc3f0741dde4bff21b213f
MattVernal <matthew.vernal@gmail.com>
2017-05-15 16:10:16 +1200

    Some more work was done

--- js/main.js
+++ js/main.js
@@ -1,40 +1,67 @@
 //Code for scrollspy
-var totalHeight = $(document).height(); 
+var totalHeight = $(document).height();
 var sectionHeight = totalHeight / 5;
 var homeHeight = 0;
 var profileHeight = sectionHeight;
 var skillsHeight = sectionHeight * 2;
 var portfolioHeight = sectionHeight * 3;
 var contactHeight = sectionHeight * 4;
+
 var activeSection = null;
 
-$(document).scroll(function(){
-    
+$(document).scroll(function () {
+    addActiveClass(activeSection);
 });
 
-$("#homeLink").click(function(){
+$("#homeLink").click(function () {
     $('html, body').animate({scrollTop: homeHeight}, 500);
 });
 
-$("#profileLink").click(function(){
+$("#profileLink").click(function () {
     $('html, body').animate({scrollTop: profileHeight}, 500);
 });
 
-$("#skillsLink").click(function(){
+$("#skillsLink").click(function () {
     $('html, body').animate({scrollTop: skillsHeight}, 500);
 });
 
-$("#portfolioLink").click(function(){
+$("#portfolioLink").click(function () {
     $('html, body').animate({scrollTop: portfolioHeight}, 500);
 });
 
-$("#contactLink").click(function(){
+$("#contactLink").click(function () {
     $('html, body').animate({scrollTop: contactHeight}, 500);
 });
 
-function addActiveClass(activeSection){
-    currentScrollHeight = $(document).offsetTop;
-    if (currentScrollHeight >= homeHeight && currentScrollHeight <= profileHeight){
+function addActiveClass(activeSection) {
+    removeActiveClass();
+    currentScrollHeight = ($('body').scrollTop() + 10);
+    if (currentScrollHeight >= homeHeight && currentScrollHeight <= profileHeight) {
         $('#homeLink').addClass("sidebar__anchors__active");
+        activeSection = $('#homeLink');
     }
+    if (currentScrollHeight >= profileHeight && currentScrollHeight <= skillsHeight) {
+        $('#profileLink').addClass("sidebar__anchors__active");
+        activeSection = $('#profileLink');
+    }
+    if (currentScrollHeight >= skillsHeight && currentScrollHeight <= portfolioHeight) {
+        $('#skillsLink').addClass("sidebar__anchors__active");
+        activeSection = $('#skillsLink');
+    }
+    if (currentScrollHeight >= portfolioHeight && currentScrollHeight <= contactHeight) {
+        $('#portfolioLink').addClass("sidebar__anchors__active");
+        activeSection = $('#portfolioLink');
+    }
+    if (currentScrollHeight >= contactHeight) {
+        $('#contactLink').addClass("sidebar__anchors__active");
+        activeSection = $('#contactLink');
+    }
+    return activeSection;
+}
+function removeActiveClass(){
+    var sections = $('.sidebar__anchors');
+    sections.each(function(){
+        $(this).removeClass('sidebar__anchors__active');
+    });
+
 }
